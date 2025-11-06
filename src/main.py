2234baf229cef3c38272f86b269e92de02445fb0
RomesAll <romesky845@gmail.com>
2025-11-07 01:28:03 +0300

    Функция для вывода средней зарплаты select_resumes_avg_compensation

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 from database import Base, async_engine, engine, session_factory
 from models import *
-from sqlalchemy import select, text, update
+from sqlalchemy import Integer, and_, cast, insert, inspect, or_, select, text, update, func
 import asyncio
-
 
 async def setup_db():
     async with async_engine.begin() as conn:
@@ -35,11 +34,47 @@ async def update_workers(id: int = 2, username: str = 'Jack_new'):
         # workers.username = username
         # await session.commit()
 
+async def insert_resumes():
+    async with session_factory() as session:
+        resume_jack_1 = ResumesORM(
+            title="Python Junior Developer", compensation=50000, workload=Workload.fulltime, worker_id=1)
+        resume_jack_2 = ResumesORM(
+            title="Python Разработчик", compensation=150000, workload=Workload.fulltime, worker_id=1)
+        resume_michael_1 = ResumesORM(
+            title="Python Data Engineer", compensation=250000, workload=Workload.parttime, worker_id=2)
+        resume_michael_2 = ResumesORM(
+            title="Data Scientist", compensation=300000, workload=Workload.fulltime, worker_id=2)
+        session.add_all([resume_jack_1, resume_jack_2, 
+                        resume_michael_1, resume_michael_2])
+        await session.commit()
+
+async def select_resumes_avg_compensation(like_language: str = "Python"):
+    """
+    select workload, avg(compensation)::int as avg_compensation
+    from resumes
+    where title like '%Python%' and compensation > 40000
+    group by workload
+    having avg(compensation) > 70000
+    """
+    async with session_factory() as session:
+        query = (
+            select(
+                ResumesORM.workload,
+                func.avg(ResumesORM.compensation).cast(Integer).label('avg_comp'))
+            .select_from(ResumesORM).filter(and_(ResumesORM.compensation > 40000, ResumesORM.title.contains(like_language)))
+            .group_by(ResumesORM.workload)
+            .having(func.avg(ResumesORM.compensation) > 70000)
+        )
+        avg_compensation = await session.execute(query)
+        result = avg_compensation.all()
+        print(result)
 
 
 async def main():
     await setup_db()
     await insert_workers()
-    await asyncio.gather(select_workers(), update_workers())
+    await insert_resumes()
+
+    await asyncio.gather(select_resumes_avg_compensation())
 
 asyncio.run(main())
